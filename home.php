0216ecdcb26092110eabb5a839e24c3b8d857c85
keninggg <146718088+keninggg@users.noreply.github.com>
2025-04-16 01:20:58 +0800

    Update content of home.php

--- home.php
+++ home.php
@@ -1,7 +1,7 @@
 <div class="hero-image">
     <div class="hero-text">
         <h1>Welcome to the <br>Recipe and Culinary Web Application</h1>
-        <p>Experience the joy of celebration with our curated engagement festival plans. Explore a variety of themes and activities to make your engagement party an unforgettable event.</p>
+       <p>Experience the joy of recipe and culinary with our web application. Explore a variety of recipes and culinary to make your life more colourful.</p><br>
         <a href="<?= $base_path ?>/recipes/index.php" class="explore-btn">EXPLORE</a>
     </div>
 </div>
