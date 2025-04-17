64c6c67e991e1451db1ebe0e3e1bd0f376f2eecd
keninggg <146718088+keninggg@users.noreply.github.com>
2025-04-17 18:47:42 +0800

    updated header

--- admin_header.php
+++ admin_header.php
@@ -14,25 +14,11 @@
     <nav>
         <ul>
             <?php $base_path = '/recipe culinary'; ?>
-            <li> <a href="<?= $base_path ?>">Home</a> </li>
-            <div class="dropdown">
-                <li> <a href="<?= $base_path ?>/recipes">Recipes</a> </li>
-                <div class="dropdown-content">
-                    <a href="<?= $base_path ?>/recipes/add_recipe.php">Create Recipe</a>
-                    <a href="<?= $base_path ?>/recipes/edit_recipe.php?recipe_id=2">Edit Recipe</a>
-                    <a href="<?= $base_path ?>/recipes/delete_recipe.php">Delete Recipe</a>
-                    <a href="<?= $base_path ?>/recipes/index.php#search-bar">Search Recipe</a>
-                </div>
-            </div>
-            <li> <a href="<?= $base_path ?>/community/discussions/list.php">Community</a> </li>
-
-            <div class="dropdown">
-                <li> <a href="<?= $base_path ?>/cooking_competition">Cooking Competition</a> </li>
-                <div class="dropdown-content">
-                    <a href="<?= $base_path ?>/cooking_competition?page=competitions&action=create">Create Competition</a>
-                    <a href="<?= $base_path ?>/cooking_competition?page=competitions">List All Competitions</a>
-                </div>
-            </div>
+            <?php if (isset($_SESSION['role']) && $_SESSION['role'] === 'admin'): ?>
+                <li> <a href="<?= $base_path ?>/users/admin_dashboard.php">Dashboard</a> </li>
+            <?php endif; ?>
+            <li> <a href="<?= $base_path ?>/users/manage_recipes.php">Recipes</a> </li>
+            <li> <a href="<?= $base_path ?>/users/manage_competition.php">Cooking Competition</a> </li>
 
             <!-- Check if the user is logged in -->
             <div class="dropdown">
